6a63a0fdc40525d8a28db2b4a34a328a778b9867
Mark Mutugi <mutugimg@gmail.com>
2023-11-08 11:46:07 +0300

    Lab complete

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,19 @@
-# your code goes here!
+import time
+
+def countdown(count):
+    int = 0
+    while int < count:
+        print(f"{count - int} SECOND(S)!")
+        int += 1
+    print("HAPPY NEW YEAR!")
+
+# countdown(10)
+
+def countdown_with_sleep(count):
+    i = 0
+    while i < count:
+        print(f"{count - i} SECOND(S)!")
+        time.sleep(1)
+        i += 1
+
+    print("HAPPY NEW YEAR!")
